b3c92d137a84cd90f3c7b613394334ec1a5c112d
wenheqi <wenhe.qi@outlook.com>
2020-07-28 21:26:43 -0700

    Add font and background color

--- src/components/header.js
+++ src/components/header.js
@@ -4,11 +4,11 @@ export default function Header() {
   return (
     <Navbar expand="lg" className="px-lg-5">
       <Navbar.Toggle aria-controls="site-navbar-nav" />
-      <Navbar.Brand href="/">YAMDB api</Navbar.Brand>
+      <Navbar.Brand href="/">🍠{` `}YamDB</Navbar.Brand>
       <Navbar.Collapse id="site-navbar-nav" className="justify-content-end">
         <Nav className="mr-0">
           <Nav.Link href="/documentation">Documentation</Nav.Link>
-          <Nav.Link href="/getkey">GET FREE API KEY</Nav.Link>
+          <Nav.Link href="/getkey">Get Free API Key</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,57 @@
 import React from "react"
+import scrollTo from "gatsby-plugin-smoothscroll"
 import Layout from "../components/layout"
 
 export default function Home() {
   return (
     <Layout>
-      <div>
-        <div className="px-3">
-          <h2>Movies, TV episodes and games</h2>
-          <h4>
-            YAMDB is a simple and lightweight API for millions of movies, TV
-            episodes and games.
-          </h4>
+      <section id="hero">
+        <div
+          className="d-flex flex-column justify-content-center align-items-center px-3 py-3 vh-100"
+          style={{ backgroundColor: "aliceblue" }}
+        >
+          <h2>
+            YamDB is a simple yet powerful API connecting millions of movies,
+            posters and more.
+          </h2>
+          <div className="d-flex flex-column justify-content-center align-items-stretch">
+            <button
+              className="btn btn-primary mt-5 mb-2"
+              onClick={() => scrollTo("#signup")}
+            >
+              GET FREE KEY
+            </button>
+            <button
+              className="btn btn-primary mt-2"
+              onClick={() => scrollTo("#documentation")}
+            >
+              LEARN MORE
+            </button>
+          </div>
         </div>
-        <div className="d-flex flex-column justify-content-center align-items-center">
-          <a>GET FREE KEY</a>
-          <a>Learn more</a>
+      </section>
+      <section
+        id="documentation"
+        className="vh-100"
+        style={{ backgroundColor: "lightyellow" }}
+      >
+        <div>
+          <code>https://api.yamdbapi.com?key=YOUR_API_KEY&id=tt0000001</code>
+          <p>Usage:</p>
+          <p>table to illustrate the usage</p>
         </div>
-      </div>
+      </section>
+      <section
+        id="signup"
+        className="vh-100"
+        style={{ backgroundColor: "paleturquoise" }}
+      >
+        <h1>All we need is just an email.</h1>
+        <form>
+          <input type="email" />
+          <button>Get Free Key</button>
+        </form>
+      </section>
     </Layout>
   )
 }
