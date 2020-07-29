aeeedf0ca2e09ba571cc1e68584ff824ed33de1e
wenheqi <wenhe.qi@outlook.com>
2020-07-29 16:14:05 -0700

    Encode mailto email address

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,12 @@
-import React from "react"
+import React, { useRef } from "react"
 import { Nav, Navbar } from "react-bootstrap"
 export default function Header() {
+  const contactEl = useRef(null)
+  const contactOnClick = e => {
+    // `current` points to the mounted anchor element
+    contactEl.current.href = `mailto:${atob(contactEl.current.href.slice(7))}`
+  }
+
   return (
     <Navbar expand="lg" className="px-lg-5">
       <Navbar.Toggle aria-controls="site-navbar-nav" />
@@ -8,6 +14,13 @@ export default function Header() {
       <Navbar.Collapse id="site-navbar-nav" className="justify-content-end">
         <Nav className="mr-0">
           <Nav.Link href="/documentation">Documentation</Nav.Link>
+          <Nav.Link
+            ref={contactEl}
+            href="mailto:d2VuaGUucWlAb3V0bG9vay5jb20="
+            onClick={contactOnClick}
+          >
+            Contact
+          </Nav.Link>
           <Nav.Link href="/getkey">Get Free API Key</Nav.Link>
         </Nav>
       </Navbar.Collapse>
--- src/components/signupform.js
+++ src/components/signupform.js
@@ -7,27 +7,31 @@ export default function SignUpForm({ className }) {
         className ? ` ${className}` : ""
       }`}
     >
-      <div class="form-group">
+      <div className="form-group">
         <input
           type="email"
-          class="form-control"
+          className="form-control"
           placeholder="Email address"
           id="signUpFormEmail"
           aria-describedby="emailHelp"
         />
         <label htmlFor="signUpFormEmail">Email address</label>
-        <small id="emailHelp" class="form-text text-muted">
+        <small id="emailHelp" className="form-text text-muted">
           We'll never share your email with anyone else.
         </small>
       </div>
-      <div class="form-group form-check">
-        <input type="checkbox" class="form-check-input" id="agreementCheck" />
-        <label class="form-check-label" htmlFor="agreementCheck">
+      <div className="form-group form-check">
+        <input
+          type="checkbox"
+          className="form-check-input"
+          id="agreementCheck"
+        />
+        <label className="form-check-label" htmlFor="agreementCheck">
           I have read and agree to the Terms of Service, Privacy Policy and
           Service Agreement of this website and service.
         </label>
       </div>
-      <button type="submit" class="btn">
+      <button type="submit" className="btn">
         Get API Key
       </button>
     </form>
