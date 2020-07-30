154a259136707f1397bc858cc0cf1b332157cda9
wenheqi <wenhe.qi@outlook.com>
2020-07-29 17:07:11 -0700

    Fix layout displaying disorder on iPad
    The footer was lifted up next to the first section with vh-100 and the navbar only got the first 1/3 displayed

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import Footer from "./footer"
 
 export default function Layout({ children }) {
   return (
-    <Container fluid className={`min-vh-100 vh-100 px-0 d-flex flex-column`}>
+    <Container fluid className={`min-vh-100 px-0 d-flex flex-column`}>
       <Row noGutters className="justify-content-center">
         <Col>
           <Header />
