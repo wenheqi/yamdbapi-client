1daf02928296786b498aa9236c4373c5b7761f88
wenheqi <wenhe.qi@outlook.com>
2020-07-30 00:07:08 -0700

    Validate user input when signup API key

--- src/components/signupform.js
+++ src/components/signupform.js
@@ -1,6 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function SignUpForm({ className }) {
+  const [email, setEmail] = useState("")
+  const [agreed, setAgreed] = useState(false)
+  const [isValidEmail, setIsValidEmail] = useState(false)
+
+  const onChangeHandler = e => {
+    setEmail(e.target.value)
+    setIsValidEmail(/^\S+@\S+\.\S+$/.test(e.target.value))
+  }
+
+  const onClickHandler = e => {
+    setAgreed(oldStatus => !oldStatus)
+  }
+
   return (
     <form
       className={`signUpForm d-flex flex-column justify-content-center align-items-stretch flex-lg-row align-items-lg-start ${
@@ -16,6 +29,8 @@ export default function SignUpForm({ className }) {
               placeholder="Email address"
               id="signUpFormEmail"
               aria-describedby="emailHelp"
+              onChange={onChangeHandler}
+              value={email}
             />
             <label htmlFor="signUpFormEmail">Email address</label>
           </div>
@@ -28,6 +43,8 @@ export default function SignUpForm({ className }) {
             type="checkbox"
             className="form-check-input"
             id="agreementCheck"
+            checked={agreed}
+            onClick={onClickHandler}
           />
           <label className="form-check-label" htmlFor="agreementCheck">
             I have read and agree to the Terms of Service, Privacy Policy and
@@ -35,7 +52,11 @@ export default function SignUpForm({ className }) {
           </label>
         </div>
       </div>
-      <button type="submit" className="btn mx-lg-2">
+      <button
+        type="submit"
+        className="btn mx-lg-2"
+        disabled={!agreed || !isValidEmail}
+      >
         Get API Key
       </button>
     </form>
