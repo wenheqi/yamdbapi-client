a1ae63e5eaff0e2380a2499027082ed2439f143c
wenheqi <wenhe.qi@outlook.com>
2020-08-01 14:23:17 -0700

    Add padding to signup page

--- src/pages/getkey.js
+++ src/pages/getkey.js
@@ -5,7 +5,7 @@ import SignUpForm from "../components/signupform"
 export default function GetKeyPage() {
   return (
     <Layout>
-      <div className="h-100 d-flex flex-column justify-content-center align-items-center pageGenKey">
+      <div className="h-100 d-flex flex-column justify-content-center align-items-center px-3 py-3 pageGenKey">
         <SignUpForm />
       </div>
     </Layout>
