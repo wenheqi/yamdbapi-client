ee70be6c51271288bc9dbe44e5adb114a9fd55a6
wenheqi <wenhe.qi@outlook.com>
2020-08-03 15:48:03 -0700

    Implement request example in Chrome

---
+++ src/components/chrome.js
@@ -0,0 +1,203 @@
+import React from "react"
+
+export default function Chrome() {
+  return (
+    <div className="chrome">
+      <div className="tabBar">
+        <div className="buttons">
+          <div className="circle sunsetOrange"></div>
+          <div className="circle pastelOrange"></div>
+          <div className="circle malachite"></div>
+        </div>
+      </div>
+      <div className="addressBar">
+        <div className="address">
+          <span className="url">
+            {`https://yamdbapi.com?apikey=YOUR_API_KEY&id=tt0848228`}
+          </span>
+        </div>
+      </div>
+      <div className="webpageWindow">
+        <pre>
+          {`{
+    "status": `}
+          {"  "}
+          <span className="green">"OK"</span>,
+          {`
+    "result": [
+      {
+        "_id": `}
+          <span className="green">"tt0848228"</span>,
+          {`
+        "name": `}
+          <span className="green">"The Avengers"</span>,
+          {`
+        "type": `}
+          <span className="green">"Movie"</span>,
+          {`
+        "year": `}
+          <span className="green">"2012"</span>,
+          {`
+        "duration": `}
+          <span className="green">"PT2H23M"</span>,
+          {`
+        "contentRating": `}
+          <span className="green">"PG-13"</span>,
+          {`
+        "poster": `}
+          <span className="green">
+            "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
+          </span>
+          ,
+          {`
+        "description": `}
+          <span className="green">
+            "The Avengers is a movie starring Robert Downey Jr., Chris Evans,
+            and Scarlett Johansson. Earth's mightiest heroes must come together
+            and learn to fight as a team if they are going to stop the
+            mischievous Loki and his alien army from..."
+          </span>
+          ,
+          {`
+        "trailerThumbnail": `}
+          <span className="green">
+            "https://m.media-amazon.com/images/M/MV5BMTcwNTg1Nzg2MF5BMl5BanBnXkFtZTcwNDEwOTUwOA@@._V1_.jpg"
+          </span>
+          ,
+          {`
+        "imdbRating": {
+          "bestRating": `}
+          <span className="green">"10.0"</span>,
+          {`
+          "ratingCount": `}
+          <span className="blue">1237044</span>,
+          {`
+          "worstRating": `}
+          <span className="green">"1.0"</span>,
+          {`
+          "ratingValue": `}
+          <span className="green">"8.0"</span>,
+          {`
+        },
+        "genres": [
+          `}
+          <span className="green">"Action"</span>,
+          {`
+          `}
+          <span className="green">"Adventure"</span>,
+          {`
+          `}
+          <span className="green">"Sci-Fi"</span>
+          {`
+        ],
+        "keywords": [
+          `}
+          <span className="green">"superhero"</span>,
+          {`
+          `}
+          <span className="green">"marvel cinematic universe"</span>,
+          {`
+          `}
+          <span className="green">"alien invasion"</span>,
+          {`
+          `}
+          <span className="green">"s.h.i.e.l.d."</span>,
+          {`
+          `}
+          <span className="green">"final battle"</span>
+          {`
+        ],
+        "actors": [
+          {
+            "name": `}
+          <span className="green">"Robert Downey Jr."</span>,
+          {`
+            "id": `}
+          <span className="green">"nm0000375"</span>
+          {`
+          },
+          {
+            "name": `}
+          <span className="green">"Chris Evans"</span>,
+          {`
+            "id": `}
+          <span className="green">"nm0262635"</span>
+          {`
+          },
+          {
+            "name": `}
+          <span className="green">"Scarlett Johansson"</span>,
+          {`
+            "id": `}
+          <span className="green">"nm0424060"</span>
+          {`
+          },
+          {
+            "name": `}
+          <span className="green">"Jeremy Renner"</span>,
+          {`
+            "id": `}
+          <span className="green">"nm0719637"</span>
+          {`
+          }
+        ],
+        "creators": [
+          {
+            "name": `}
+          <span className="green">"Joss Whedon"</span>,
+          {`
+            "id": `}
+          <span className="green">"nm0923736"</span>
+          {`
+          },
+          {
+            "name": `}
+          <span className="green">"Zak Penn"</span>,
+          {`
+            "id": `}
+          <span className="green">"nm0672015"</span>
+          {`
+          },
+          {
+            "name": `}
+          <span className="green">"Joss Whedon"</span>,
+          {`
+            "id": `}
+          <span className="green">"nm0923736"</span>
+          {`
+          },
+          {
+            "name": `}
+          <span className="green">"Organization"</span>,
+          {`
+            "id": `}
+          <span className="green">"co0051941"</span>
+          {`
+          },
+          {
+            "name": `}
+          <span className="green">"Organization"</span>,
+          {`
+            "id": `}
+          <span className="green">"co0023400"</span>
+          {`
+          }
+        ],
+        "directors": [
+          {
+            "name" `}
+          <span className="green">"Joss Whedon"</span>,
+          {`
+              "id": `}
+          <span className="green">"nm0923736"</span>
+          {`
+          }
+        ]
+      }
+    ]
+}`}
+        </pre>
+      </div>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import scrollTo from "gatsby-plugin-smoothscroll"
 import Layout from "../components/layout"
 import SignUpForm from "../components/signupform"
 import SEO from "../components/seo"
+import Chrome from "../components/chrome"
 
 export default function Home() {
   return (
@@ -35,107 +36,18 @@ export default function Home() {
       </section>
       <section id="usage">
         <div
-          className="min-vh-100 px-2 py-4"
+          className="vh-100 d-flex flex-column flex-lg-row px-2 py-4"
           style={{ backgroundColor: "lightyellow" }}
         >
-          <h2>YamDB API is super easy to use.</h2>
-          <p>
-            Append query parameters along with API key to the HTTP endpoint,
-            YamDB will return all the data fulfill your query in JSON format.
-          </p>
-          <p>For example:</p>
-          <code className="d-block px-2 py-2 mb-3">
-            {/* TODO: replace with real request URL */}
-            {`https://www.yamdbapi.com?key=YOUR_API_KEY&id=tt7653254`}
-          </code>
-          <p>returns</p>
-          <code className="d-block px-2 py-2 mb-3">
-            {/* TODO: replace with real response, should have status and an array */}
-            <pre>
-              {`{
-  "@context": "http://schema.org",
-  "@type": "Movie",
-  "url": "/title/tt7653254/",
-  "name": "Marriage Story",
-  "image": "https://m.media-amazon.com/images/M/MV5BZGVmY2RjNDgtMTc3Yy00YmY0LTgwODItYzBjNWJhNTRlYjdkXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_.jpg",
-  "genre": [
-    "Comedy",
-    "Drama",
-    "Romance"
-  ],
-  "contentRating": "R",
-  "actor": [
-    {
-      "@type": "Person",
-      "url": "/name/nm3485845/",
-      "name": "Adam Driver"
-    },
-    {
-      "@type": "Person",
-      "url": "/name/nm0424060/",
-      "name": "Scarlett Johansson"
-    },
-    {
-      "@type": "Person",
-      "url": "/name/nm5005121/",
-      "name": "Julia Greer"
-    },
-    {
-      "@type": "Person",
-      "url": "/name/nm7309485/",
-      "name": "Azhy Robertson"
-    }
-  ],
-  "director": {
-    "@type": "Person",
-    "url": "/name/nm0000876/",
-    "name": "Noah Baumbach"
-  },
-  "creator": [
-    {
-      "@type": "Person",
-      "url": "/name/nm0000876/",
-      "name": "Noah Baumbach"
-    },
-    {
-      "@type": "Organization",
-      "url": "/company/co0159772/"
-    },
-    {
-      "@type": "Organization",
-      "url": "/company/co0144901/"
-    }
-  ],
-  "description": "Marriage Story is a movie starring Adam Driver, Scarlett Johansson, and Julia Greer. Noah Baumbach\u0027s incisive and compassionate look at a marriage breaking up and a family staying together.",
-  "datePublished": "2019-11-28",
-  "keywords": "infidelity,new york city,brooklyn new york city,los angeles california,actress",
-  "aggregateRating": {
-    "@type": "AggregateRating",
-    "ratingCount": 222731,
-    "bestRating": "10.0",
-    "worstRating": "1.0",
-    "ratingValue": "8.0"
-  },
-  "duration": "PT2H17M",
-  "trailer": {
-    "@type": "VideoObject",
-    "name": "Official Trailer",
-    "embedUrl": "/video/imdb/vi2650521369",
-    "thumbnail": {
-      "@type": "ImageObject",
-      "contentUrl": "https://m.media-amazon.com/images/M/MV5BYWE2N2JlZGYtYTNjZC00NDY1LThjZjctYzBjOTdiMmE1YzY4XkEyXkFqcGdeQWFybm8@._V1_.jpg"
-    },
-    "thumbnailUrl": "https://m.media-amazon.com/images/M/MV5BYWE2N2JlZGYtYTNjZC00NDY1LThjZjctYzBjOTdiMmE1YzY4XkEyXkFqcGdeQWFybm8@._V1_.jpg",
-    "description": "An incisive and compassionate portrait of a marriage breaking up and a family staying together. From Academy Award-nominated filmmaker Noah Baumbach and starring Scarlett Johansson, Adam Driver, Laura Dern, Alan Alda and Ray Liotta.",
-    "uploadDate": "2019-10-17T23:31:51Z"
-  }
-}`}
-            </pre>
-          </code>
-          <div className="d-flex flex-column justify-content-center align-items-center py-5">
-            <a href="/documentation">
-              <button className="btn">See Full Parameters</button>
-            </a>
+          <div className="d-flex flex-column flex-lg-shrink-1 justify-content-center">
+            <h2>YamDB API is super easy to use.</h2>
+            <p>
+              Append query parameters along with API key to the HTTP endpoint,
+              YamDB will return all the data fulfill your query in JSON format.
+            </p>
+          </div>
+          <div className="flex-grow-1 d-flex flex-column justify-content-center align-items-center p-lg-5 overflow-auto">
+            <Chrome />
           </div>
         </div>
       </section>
